d531f5a36c62695192173d40940fa594dcb500ed
Enkegithub <niteshkushwaha484@gmail.com>
2026-06-11 16:21:21 +0530

    speedinsithst add

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import '../styles.css';
 import Script from 'next/script';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata = {
   title: 'Nitesh | Full-Stack Developer',
   description: 'Portfolio of Nitesh Kushwaha, Full-Stack Developer, Android Developer, Flutter Developer and AI Enthusiast',
